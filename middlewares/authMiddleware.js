006f946376996647ef17fee47165ba2c8ef97f2b
AchaUmbaPretre <achandambi@gmail.com>
2024-12-06 11:38:43 +0100

    commit

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwtUtils = require('../utils/jwt');
+
+exports.authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.status(401).json({ error: 'Token manquant' });
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const user = jwtUtils.verifyAccessToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: 'Token invalide ou expiré' });
+  }
+};
