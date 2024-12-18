0d0a82630517d042651716886aea7c5f5ba70934
AchaUmbaPretre <achandambi@gmail.com>
2024-12-18 13:51:46 +0100

    commit

--- controllers/type.controller.js
+++ controllers/type.controller.js
@@ -12,3 +12,21 @@ const queryAsync = (query, values = []) =>
         });
     });
 
+exports.getCatPermis = async (req, res) => {
+    try {
+        const query = `SELECT * FROM cat_permis`;
+    
+        const chauffeurs = await queryAsync(query);
+        
+        return res.status(200).json({
+            message: 'Liste des cat permis récupérée avec succès',
+            data: chauffeurs,
+        });
+    } catch (error) {
+        console.error('Erreur lors de la récupération des cat permis :', error);
+        
+        return res.status(500).json({
+                    error: "Une erreur s'est produite lors de la récupération des chauffeurs.",
+        });
+    }
+}
