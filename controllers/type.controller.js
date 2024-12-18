37942ada00f2fb273f8511bde8809c5eccb0affb
AchaUmbaPretre <achandambi@gmail.com>
2024-12-18 15:28:54 +0100

    commit

--- controllers/type.controller.js
+++ controllers/type.controller.js
@@ -83,7 +83,7 @@ exports.getTypeFonction = async (req, res) => {
         console.error('Erreur lors de la récupération des cat permis :', error);
         
         return res.status(500).json({
-                    error: "Une erreur s'est produite lors de la récupération des chauffeurs.",
+            error: "Une erreur s'est produite lors de la récupération des chauffeurs.",
         });
     }
 }
--- routes/type.routes.js
+++ routes/type.routes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const  typeController = require('./../controllers/type.controller');
 
-router.get('/cat_permis', typeController.getCatPermis)
+router.get('/cat_permis', typeController.getCatPermis);
 router.get('/type_contrat', typeController.getTypeContrat);
 router.get('/etat_civil', typeController.getEtatCivil);
+router.get('/type_fonction', typeController.getTypeFonction);
+
 
 module.exports = router;
