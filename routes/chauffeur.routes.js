dddb9212a389d58c7647700934a2d8394e320a6c
AchaUmbaPretre <achandambi@gmail.com>
2024-12-18 13:23:39 +0100

    commit

--- routes/chauffeur.routes.js
+++ routes/chauffeur.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const  chauffeurController = require('./../controllers/chauffeur.controller');
 
+router.get('/count', chauffeurController.getChauffeurCount)
 router.get('/', chauffeurController.getChauffeur)
 router.post('/', chauffeurController.postChauffeur);
 
