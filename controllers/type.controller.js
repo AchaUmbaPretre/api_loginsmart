c73576202fc9e7dd5976caeb3fc20a087628fa0b
AchaUmbaPretre <achandambi@gmail.com>
2024-12-18 13:48:18 +0100

    commit

---
+++ controllers/type.controller.js
@@ -0,0 +1,14 @@
+const { db } = require("./../config/database");
+const { validationResult } = require('express-validator');
+
+
+const queryAsync = (query, values = []) =>
+    new Promise((resolve, reject) => {
+        db.query(query, values, (error, results) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve(results);
+        });
+    });
+
---
+++ routes/type.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const  typeController = require('./../controllers/type.controller');
+
+/* router.get('/count',)
+router.get('/',)
+router.post('/', chauffeurController.postChauffeur); */
+
+module.exports = router;
