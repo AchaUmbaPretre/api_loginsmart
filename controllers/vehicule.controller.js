b346f689979d432469d7c027bbf441df09cafcf4
AchaUmbaPretre <achandambi@gmail.com>
2024-12-19 12:41:24 +0100

    commit

--- controllers/vehicule.controller.js
+++ controllers/vehicule.controller.js
@@ -26,8 +26,8 @@ exports.getVehiculeCount = async (req, res) => {
         return res.status(500).json({
             message: "Une erreur s'est produite lors de la récupération des chauffeurs.",
             error: error.message,
-          });
-        }
+        });
+    }
 };
 
 
@@ -48,4 +48,96 @@ exports.getVehicule = async (req, res) => {
                 error: "Une erreur s'est produite lors de la récupération des chauffeurs.",
             });
         }
+};
+
+exports.postVehicule = async (req, res) => {
+    
+    try {
+        if (!req.files || req.files.length === 0) {
+            return res.status(400).json({ error: 'Aucun fichier téléchargé' });
+        }
+
+        const profil = req.files.map((file) => file.path.replace(/\\/g, '/')).join(',');
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {
+            immatriculation,
+            numero_ordre,
+            id_marque,
+            id_modele,
+            variante,
+            num_chassis,
+            annee_fabrication,
+            annee_circulation,
+            id_cat_vehicule,
+            id_type_permis_vehicule,
+            img,
+            longueur,
+            largeur,
+            hauteur,
+            poids,
+            id_couleur,
+            capacite_carburant,
+            capacite_radiateur,
+            capacite_carter,
+            nbre_place,
+            nbre_portes,
+            nbre_moteur,
+            cylindre,
+            nbre_cylindre,
+            disposition_cylindre,
+            id_type_carburant,
+            regime_moteur_vehicule,
+            consommation_carburant,
+            turbo,
+            date_service,
+            km_initial,
+            nbre_chev,
+            id_transmission,
+            id_climatisation,
+            pneus,
+            valeur_acquisition,
+            lubrifiant_moteur,
+            id_etat,
+            user_cr
+        } = req.body;
+
+        const query = `
+            INSERT INTO vehicules (
+                immatriculation, numero_ordre, id_marque, id_modele, variante, num_chassis,
+                annee_fabrication, annee_circulation, id_cat_vehicule, id_type_permis_vehicule, img,
+                longueur, largeur, hauteur, poids, id_couleur, capacite_carburant, capacite_radiateur,
+                capacite_carter, nbre_place, nbre_portes, nbre_moteur, cylindre, nbre_cylindre, disposition_cylindre,
+                id_type_carburant, regime_moteur_vehicule, consommation_carburant, turbo, date_service, km_initial, nbre_chev, 
+                id_transmission, id_climatisation, pneus, valeur_acquisition, lubrifiant_moteur, id_etat, user_cr
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+
+        const values = [
+            matricule, nom, prenom, telephone, adresse, id_etat_civil,
+            statut, profil, sexe, id_type_contrat, type_travail,
+            id_permis, id_ville, date_naissance, date_engagement, user_cr, tel_service,
+        ];
+
+        const result = await queryAsync(query, values);
+
+        return res.status(201).json({
+            message: 'Chauffeur ajouté avec succès',
+            data: { id: result.insertId, nom, prenom },
+        });
+    } catch (error) {
+        console.error('Erreur lors de l’ajout du chauffeur :', error);
+
+        const statusCode = error.code === 'ER_DUP_ENTRY' ? 409 : 500;
+        const errorMessage =
+            error.code === 'ER_DUP_ENTRY'
+                ? "Un chauffeur avec ces informations existe déjà."
+                : "Une erreur s'est produite lors de l'ajout du chauffeur.";
+
+        return res.status(statusCode).json({ error: errorMessage });
+    }
 };
