b87aec2636f077ed4e0e3554405e1dc39cfdfd34
AchaUmbaPretre <achandambi@gmail.com>
2024-12-23 14:36:35 +0100

    commit

--- controllers/carburant.controller.js
+++ controllers/carburant.controller.js
@@ -35,11 +35,60 @@ exports.getCarburantCount = async (req, res) => {
 exports.getCarburant = async (req, res) => {
 
     try {
-        const query = `SELECT plein.id_plein, plein.qte_plein, plein.kilometrage, plein.matricule_ch, plein.observation, plein.date_plein, u.nom, v.immatriculation, c.nom AS nom_chauffeur, m.nom_marque FROM plein
+        const query = `SELECT plein.id_plein, plein.qte_plein, plein.kilometrage, plein.matricule_ch, plein.observation, plein.date_plein, u.nom, v.immatriculation, c.nom AS nom_chauffeur, m.nom_marque, tc.nom_type_carburant FROM plein
                             INNER JOIN vehicules v ON plein.immatriculation = v.id_vehicule
                             INNER JOIN users u ON plein.id_user = u.id
                             INNER JOIN chauffeurs c ON plein.id_chauffeur = c.id_chauffeur
-                            INNER JOIN marque m ON v.id_marque = m.id_marque`;
+                            INNER JOIN marque m ON v.id_marque = m.id_marque
+                            INNER JOIN type_carburant tc ON plein.type_carburant = tc.id_type_carburant`;
+
+            const chauffeurs = await queryAsync(query);
+    
+            return res.status(200).json({
+                message: 'Liste des véhicules pleins récupérés avec succès',
+                data: chauffeurs,
+            });
+        } catch (error) {
+            console.error('Erreur lors de la récupération des chauffeurs :', error);
+    
+            return res.status(500).json({
+                error: "Une erreur s'est produite lors de la récupération des chauffeurs.",
+            });
+        }
+};
+
+
+exports.getCarburantCinq = async (req, res) => {
+
+    try {
+        const query = `SELECT 
+                plein.id_plein, 
+                plein.qte_plein, 
+                plein.kilometrage, 
+                plein.matricule_ch, 
+                plein.observation, 
+                plein.date_plein, 
+                u.nom AS nom_utilisateur, 
+                v.immatriculation, 
+                c.nom AS nom_chauffeur, 
+                m.nom_marque, 
+                tc.nom_type_carburant
+            FROM 
+                plein
+            INNER JOIN 
+                vehicules v ON plein.immatriculation = v.id_vehicule
+            INNER JOIN 
+                users u ON plein.id_user = u.id
+            INNER JOIN 
+                chauffeurs c ON plein.id_chauffeur = c.id_chauffeur
+            INNER JOIN 
+                marque m ON v.id_marque = m.id_marque
+            INNER JOIN 
+                type_carburant tc ON plein.type_carburant = tc.id_type_carburant
+            ORDER BY 
+                plein.date_plein DESC
+            LIMIT 5;
+                `;
 
             const chauffeurs = await queryAsync(query);
     
