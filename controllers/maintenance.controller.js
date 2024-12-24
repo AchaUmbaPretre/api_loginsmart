42ba287af2512537c7af149d9930ed7214fa4c52
AchaUmbaPretre <achandambi@gmail.com>
2024-12-24 10:37:24 +0100

    commit

---
+++ controllers/maintenance.controller.js
@@ -0,0 +1,12 @@
+const { db } = require("./../config/database");
+const { validationResult } = require('express-validator');
+
+const queryAsync = (query, values = []) =>
+    new Promise((resolve, reject) => {
+        db.query(query, values, (error, results) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve(results);
+        });
+    });
