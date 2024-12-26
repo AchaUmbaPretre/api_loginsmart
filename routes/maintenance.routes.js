1133c20b25cf9d210274ac8cb1b1d7e56ee550c3
AchaUmbaPretre <achandambi@gmail.com>
2024-12-26 16:59:29 +0100

    commit

--- routes/maintenance.routes.js
+++ routes/maintenance.routes.js
@@ -6,6 +6,8 @@ router.get('/reparation', maintenantController.getReparation)
 router.post('/reparation', maintenantController.postReparation)
 
 router.get('/suivi', maintenantController.getSuivi)
+router.get('/suivi/one', maintenantController.getSuiviOne)
+
 router.post('/suivi', maintenantController.postSuivi)
 
 module.exports = router;
