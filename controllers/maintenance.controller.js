f4fe92f18efa867fee81d852fd88e9f2b2d5d2f1
AchaUmbaPretre <achandambi@gmail.com>
2024-12-27 09:00:14 +0100

    commit

--- controllers/maintenance.controller.js
+++ controllers/maintenance.controller.js
@@ -291,7 +291,7 @@ exports.getSuiviOneReparation = async (req, res) => {
     const { id_reparation} = req.query;
 
         try {
-            const query = `SELECT sr.id_suivi_reparation, sr.cout, sr.description, r.immatriculation, tt.type_tache FROM suivi_reparation sr
+            const query =   `SELECT sr.id_suivi_reparation, sr.cout, sr.description, r.immatriculation, tt.type_tache FROM suivi_reparation sr
                                 INNER JOIN reparations r ON sr.id_reparation = r.id_reparation
                                 INNER JOIN type_tache tt ON sr.id_tache = tt.id_type_tache
                                 INNER JOIN categorie_pieces cp ON sr.id_piece = cp.id
