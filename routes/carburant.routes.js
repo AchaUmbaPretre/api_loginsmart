e08fd167c6133c3ce095bc94ec232bfa56d1ad62
AchaUmbaPretre <achandambi@gmail.com>
2024-12-30 11:55:31 +0100

    commit

--- routes/carburant.routes.js
+++ routes/carburant.routes.js
@@ -5,6 +5,7 @@ const carburantController = require('./../controllers/carburant.controller');
 
 router.get('/count', carburantController.getCarburantCount);
 router.get('/', carburantController.getCarburant);
+router.get('/consommation', carburantController.getCarburantConsomm);
 router.get('/one', carburantController.getCarburantOne);
 router.get('/cinq_derniers', carburantController.getCarburantCinq)
 router.post('/', carburantController.postCarburant);
